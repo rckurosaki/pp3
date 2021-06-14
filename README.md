4571d63e004275a637c883ee2dfefa7e46043093
Gabriel de Paula <ter_gabriel.lima@tempotem.com.br>
2021-06-14 09:58:32 -0300

    Criação de documentação, comentários no código e algumas correções

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# pp3
-Projeto Prático 3 PG 
+# Projeto Prático 3
+
+Projeto Prático 3 (PP3) da disciplina Processamento Gráfico do curso de Ciências da Computação da Universidade Federal de São Carlos campus Sorocaba.
+Ministrada pela Profº Drº Mário Liziér.
+
+### Integrantes do grupo:
+- Gabriel de Paula e Lima - 587710
+- Renato Araujo Rizzo - 587788
+- Renato Candido Kurosaki - 587834
+
+# Introdução
+
+Objetivo desse projeto prático foi aplicar conhecimentos adquiridos na disciplina de processamento gráfico como 
+renderização, redimensionamento e rasterização de objetos 3D através de sua implementação e exibição do objeto gerado. 
+
+Os passos de implementação foram ler um objeto 3D com seus vértices e faces descritos em arquivo de objeto simples, 
+gerar uma janela adicionando esse objeto para exibição, aplicar transformação de matrizes para possibilitar movimentação desse objeto
+nos eixos x e y através de sliders controlados pelo usuário e por fim rasterizar o objeto com uma cor sólida.
+
+# Como usar
+
+Projeto implementado utilizando linguagem Java com bibliotecas auxiliares para geração do painel e sliders usados para 
+exibição e manipulação do objeto. 
+
+Desenvolvemos utilizando a IDE IntelliJ e para rodar o projeto basta abri-lo nessa ferramenta, compilar e rodar.
+
+Também é possível rodar diretamente pelo terminal, nesse caso basta entrar na pasta src e executar os comandos abaixo:
+
+```java
+javac Face.java Vertice.java ObjLoader.java Matriz.java Main.vava 
+```
+```java
+java Main 
+```
+
+Se tudo ocorrer certo, será aberto uma nova janela exibindo o objeto 3D.
+
+# Desenvolvimento e implementação
+
+Com base no arquivo de objeto disponibilizado que contém lista de pontos para representar vértices e faces, 
+iniciamos criando classes para facilitar essa representação e também criamos uma classe para "carregar" o objeto.
+Essa classe tem responsabilidade de ler o arquivo do objeto 3D, instânciar vértices e inclui-los em uma lista de faces 
+referentes aos valores obtidos nesse arquivo. Após esse carregamento, a lista de faces que representa o objeto 3D é retornada.
+
+Para efetuar a manipulação do objeto nos eixos x e y, utilizamos matrizes "in-code" e criamos a classe Matriz para auxiliar na multiplicação e transformação 
+da lista de vértices. A ideia aqui também foi pré computar a multiplicação de matriz para utilizar na rotação esquerda-direita e cima-baixo.
+Interligamos os métodos da transformação aos sliders que são instânciados na criação da janela.
+
+Para a coloração do objeto (coloração das faces) utilizamos a ideia de rasterização por coordenadas baricêntricas, onde computamos
+coordenadas para cada `pixel` que estão dentro de cada face até colorir todas.
+
+
+
+# Referências 
+
+- [Biblioteca JPanel](https://docs.oracle.com/javase/7/docs/api/javax/swing/JPanel.html)
+- [Matrix Transformation](https://www.scilab.org/tutorials/computer-vision-%E2%80%93-image-transform)
+- [Working with Images in Java](https://docs.oracle.com/javase/tutorial/2d/images/index.html)
+- [Barycentric rasterization](https://www.scratchapixel.com/lessons/3d-basic-rendering/rasterization-practical-implementation/rasterization-stage)
--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
         ObjLoader fr = new ObjLoader();
         ArrayList<Face> face_list = fr.read_file("files/coarseTri.rockerArm.obj"); //coarseTri.rockerArm.obj
 
+        // Create new window with sliders
         JFrame frame = new JFrame();
         Container pane = frame.getContentPane();
         pane.setLayout(new BorderLayout());
@@ -22,13 +23,14 @@ public class Main {
         JSlider pSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
         pane.add(pSlider, BorderLayout.EAST);
 
+        // Starts process to render object in the new window
         JPanel renderPanel = new JPanel() {
             public void paintComponent(Graphics g) {
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.DARK_GRAY);
                 g2.fillRect(0, 0, getWidth(), getHeight());
 
-                // vertical movement
+                // Vertical object movement
                 double heading = Math.toRadians(slider.getValue());
                 Matriz hTransform = new Matriz(new double[]{
                         Math.cos(heading), 0, -Math.sin(heading),
@@ -36,7 +38,7 @@ public class Main {
                         Math.sin(heading), 0, Math.cos(heading)
                 });
 
-                // horizontal movement
+                // Horizontal object movement
                 double pitch = Math.toRadians(pSlider.getValue());
                 Matriz pTransform = new Matriz(new double[]{
                         1, 0, 0,
@@ -46,7 +48,7 @@ public class Main {
 
                 Matriz transform = hTransform.multiply(pTransform);
 
-                // rendering here
+                // Rendering object
                 BufferedImage img =
                     new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
 
@@ -54,8 +56,7 @@ public class Main {
                     Vertice v1 = transform.transform(f.getV1());
                     Vertice v2 = transform.transform(f.getV2());
                     Vertice v3 = transform.transform(f.getV3());
-                    // since we are not using Graphics2D anymore,
-                    // we have to do translation manually
+
                     v1.x += getWidth() / 2.0;
                     v1.y += getHeight() / 2.0;
                     v2.x += getWidth() / 2.0;
@@ -63,25 +64,23 @@ public class Main {
                     v3.x += getWidth() / 2.0;
                     v3.y += getHeight() / 2.0;
 
-                    // compute rectangular bounds for triangle
+                    // Compute rectangular bounds for faces
                     int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
-                    int maxX = (int) Math.min(img.getWidth() - 1,
-                            Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+                    int maxX = (int) Math.min(img.getWidth() - 1, Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
                     int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
-                    int maxY = (int) Math.min(img.getHeight() - 1,
-                            Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+                    int maxY = (int) Math.min(img.getHeight() - 1, Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
 
-                    double triangleArea =
+                    double faceArea =
                             (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
 
                     for (int y = minY; y <= maxY; y++) {
                         for (int x = minX; x <= maxX; x++) {
                             double b1 =
-                                    ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+                                    ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / faceArea;
                             double b2 =
-                                    ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+                                    ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / faceArea;
                             double b3 =
-                                    ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+                                    ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / faceArea;
                             if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
                                 img.setRGB(x, y, f.color.getRGB());
                             }
